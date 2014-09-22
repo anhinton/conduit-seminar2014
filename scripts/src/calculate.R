df76d6cf4bf5f9562025dda44eb050a442ed28ad
Ashley Noel Hinton <ashley@canadia.co.nz>
2014-09-23 11:49:36 +1200

    a working page

--- scripts/src/calculate.R
+++
@@ -1,9 +0,0 @@
-
-youngNonvoters <- nonvoters$X2011[nonvoters$AgeGroup == "18 to 24"]
-
-youngPop <- sum(pop2013$Value[pop2013$numericAge >= 18 &
-                              pop2013$numericAge <= 24])
-votePop <- sum(pop2013$Value[pop2013$numericAge >= 18])
-
-youngPopNonvoters <- youngNonvoters/100*youngPop
-youngNonvotersPercent <- 100*youngPopNonvoters/votePop
